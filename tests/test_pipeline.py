35721663bffd105db0d815cabf84802ce584369d
Matheus Costa <matheeusferreeira@hotmail.com>
2023-11-12 15:15:14 -0300

    transform script tested and working

---
+++ tests/test_pipeline.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from src.ETL.transform import concat_dataframes
+
+df_1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+df_2 = pd.DataFrame({'col1': [5, 6], 'col2': [7, 8]})
+
+
+def testar_a_concatenacao_da_lista_de_dataframes():
+    """Usando arrange, act e assert para testar a função concact_data_frames"""
+    # arrange
+    df_list = [df_1, df_2]
+
+    # act
+    df = concat_dataframes(df_list)
+
+    assert df.shape == (4, 2)
+    assert df.equals(pd.concat(df_list, axis=0, ignore_index=True))
